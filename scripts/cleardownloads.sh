47bf729eef26dd9d5a14af178b17ec64a3acc63e
Alexander Sazonov <35430795+karl93rus@users.noreply.github.com>
2019-09-23 14:07:53 +0300

    Create cleardownloads.sh

---
+++ scripts/cleardownloads.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+date=$(date '+%Y-%m-%d_%H-%M')
+mkdir ~/Downloads/$date
+
+for entry in  ~/Downloads/*; do
+ #echo $entry
+ if [[ -f $entry ]]; then
+   mv $entry ~/Downloads/$date
+ else
+   echo "Not file"
+ fi
+done
